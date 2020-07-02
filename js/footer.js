b23b24dc4b690131bdc590c72bb6f3f303e84718
Adriana Garzón <adrianamgarzonp4@gmail.com>
2020-07-01 17:27:35 -0700

    Replace all files for new version that is responsive

---
+++ js/footer.js
@@ -0,0 +1,67 @@
+/*
+div.footer-hover:hover {
+    transform: scale(1.5);
+    color: var(--dark-grey, #404040);
+    box-shadow: 0 5px 10px rgba(0,0,0,0.19), 0 3px 6px rgba(0,0,0,0.23);
+}
+
+footer section ul li a div.footer-option-box p:hover {
+    color: var(--dark-grey, #404040);
+}
+*/
+
+//Footer interactions
+
+/////Linkedin
+$("#footer-item1").on({
+mouseenter: function(){
+    $("#footer-hover1").css({
+        'transform' : 'scale(1.5)',
+        'box-shadow' : '0 5px 10px rgba(0,0,0,0.19), 0 3px 6px rgba(0,0,0,0.23)'});
+    $("#footer-label1").css({
+        'color' : 'var(--black, #000000)'});
+    },
+mouseleave: function(){
+    $("#footer-hover1").css({
+        'transform' : 'none',
+        'box-shadow' : 'none'});
+    $("#footer-label1").css({
+        'color' : 'var(--blue, #3a4fab)'});
+    }
+});
+
+/////Email
+$("#footer-item2").on({
+    mouseenter: function(){
+        $("#footer-hover2").css({
+            'transform' : 'scale(1.5)',
+            'box-shadow' : '0 5px 10px rgba(0,0,0,0.19), 0 3px 6px rgba(0,0,0,0.23)'});
+        $("#footer-label2").css({
+            'color' : 'var(--black, #000000)'});
+        },
+    mouseleave: function(){
+        $("#footer-hover2").css({
+            'transform' : 'none',
+            'box-shadow' : 'none'});
+        $("#footer-label2").css({
+            'color' : 'var(--blue, #3a4fab)'});
+        }
+    });
+
+/////Resume
+$("#footer-item3").on({
+    mouseenter: function(){
+        $("#footer-hover3").css({
+            'transform' : 'scale(1.5)',
+            'box-shadow' : '0 5px 10px rgba(0,0,0,0.19), 0 3px 6px rgba(0,0,0,0.23)'});
+        $("#footer-label3").css({
+            'color' : 'var(--black, #000000)'});
+        },
+    mouseleave: function(){
+        $("#footer-hover3").css({
+            'transform' : 'none',
+            'box-shadow' : 'none'});
+        $("#footer-label3").css({
+            'color' : 'var(--blue, #3a4fab)'});
+        }
+    });
---
+++ js/mycards.js
@@ -0,0 +1,70 @@
+
+//Card interactions
+
+/////card1
+$("#card1").on({
+    click: function(){
+      $(this).css('transform', 'scale(0.8)');
+    } 
+});
+
+$("#card1").on({
+mouseenter: function(){
+    $("#lh1").css({
+    'color' : 'var(--black, #000000)',
+    'box-shadow' : '0 5px 10px rgba(0,0,0,0.19), 0 3px 6px rgba(0,0,0,0.23)',
+    'background-color' : 'var(--yellow, #f1e72f)'});
+},
+mouseleave: function(){
+    $("#lh1").css({
+        'color' : 'var(--blue, #3a4fab)',
+        'box-shadow' : 'none',
+        'background-color' : 'transparent'});
+}
+});
+
+
+/////card2
+$("#card2").on({
+    click: function(){
+      $(this).css('transform', 'scale(0.8)');
+    } 
+});
+
+$("#card2").on({
+mouseenter: function(){
+    $("#lh2").css({
+    'color' : 'var(--black, #000000)',
+    'box-shadow' : '0 5px 10px rgba(0,0,0,0.19), 0 3px 6px rgba(0,0,0,0.23)',
+    'background-color' : 'var(--yellow, #f1e72f)'});
+},
+mouseleave: function(){
+    $("#lh2").css({
+        'color' : 'var(--blue, #3a4fab)',
+        'box-shadow' : 'none',
+        'background-color' : 'transparent'});
+}
+});
+
+
+/////card 3
+$("#card3").on({
+    click: function(){
+      $(this).css('transform', 'scale(0.8)');
+    } 
+});
+
+$("#card3").on({
+mouseenter: function(){
+    $("#lh3").css({
+    'color' : 'var(--black, #000000)',
+    'box-shadow' : '0 5px 10px rgba(0,0,0,0.19), 0 3px 6px rgba(0,0,0,0.23)',
+    'background-color' : 'var(--yellow, #f1e72f)'});
+},
+mouseleave: function(){
+    $("#lh3").css({
+        'color' : 'var(--blue, #3a4fab)',
+        'box-shadow' : 'none',
+        'background-color' : 'transparent'});
+}
+});
